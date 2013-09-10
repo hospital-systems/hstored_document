6d970b8d01006e7d5cc01b82e9e95f845eea56d9
Sasha A <elequtree@gmail.com>
2013-09-10 16:03:35 +0400

    Refactoring

--- lib/hstored_document/storage.rb
+++ lib/hstored_document/storage.rb
@@ -1,29 +1,35 @@
 module HstoredDocument
   class Storage
-    class << self; include HstoredDocument::Converter; end
+    class << self
+      include HstoredDocument::Converter
 
-    def self.table_name=(tname)
-      @storage = Class.new(ActiveRecord::Base) do
-        self.table_name = tname
-        serialize :attrs, ActiveRecord::Coders::Hstore
-      end
-    end
-
-    def self.save(hash)
-      records = destruct_hash(hash)
-      records.each do |rec|
-        @storage.create rec do |object|
-          object.id = rec[:id]
+      def table_name=(tname)
+        @storage = Class.new(ActiveRecord::Base) do
+          self.table_name = tname
+          serialize :attrs, ActiveRecord::Coders::Hstore
         end
       end
-      records.first.try(:[], :agg_id)
-    end
 
-    def self.find(id)
-      x = @storage.where(agg_id: id).map do |item|
-        item.attributes.symbolize_keys
+      def save(hash)
+        records = destruct_hash(hash)
+        records.each do |rec|
+          @storage.create rec do |object|
+            object.id = rec[:id]
+          end
+        end
+        records.first.try(:[], :agg_id)
       end
-      construct_hash(x)
+
+      def find(id)
+        construct(@storage.where(agg_id: id))
+      end
+
+      def construct(records)
+        rows = records.map do |item|
+          item.attributes.symbolize_keys
+        end
+        construct_hash(rows)
+      end
     end
   end
 end
